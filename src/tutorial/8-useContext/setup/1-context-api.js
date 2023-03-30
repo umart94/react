7d70ded7d4d6b2e0b49255096880830a5578de1f
umart94 <umartariqx94@gmail.com>
2023-03-30 20:57:13 +0500

    initial commit

---
+++ src/tutorial/8-useContext/setup/1-context-api.js
@@ -0,0 +1,62 @@
+import React, { useState, useContext } from 'react';
+import { data } from '../../../data';
+// more components
+// fix - context api, redux (for more complex cases)
+
+
+const PersonContext = React.createContext();
+
+// create context gives access two components
+//provider
+//consumer
+
+
+
+
+const ContextAPI = () => {
+  const [people, setPeople] = useState(data);
+  const removePerson = (id) => {
+    setPeople((people) => {
+      return people.filter((person) => person.id !== id);
+    });
+  };
+  return (
+    <PersonContext.Provider value={{ removePerson, people }}>
+      <h3>prop drilling</h3>
+      <List />
+    </PersonContext.Provider>
+  );
+};
+
+const List = ({ }) => {
+
+  const mainData = useContext(PersonContext);
+  return (
+    <>
+      {mainData.people.map((person) => {
+        return (
+          <SinglePerson
+            key={person.id}
+            {...person}
+
+          />
+        );
+      })}
+    </>
+  );
+};
+
+const SinglePerson = ({ id, name }) => {
+
+  const { removePerson } = useContext(PersonContext);
+
+
+  return (
+    <div className='item'>
+      <h4>{name}</h4>
+      <button onClick={() => removePerson(id)}>remove</button>
+    </div>
+  );
+};
+
+export default ContextAPI;
